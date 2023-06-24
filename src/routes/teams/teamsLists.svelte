d289841f48e8549ceeca9d3c925aaea5d0c72038
Azran Hossain <mr69751@gmail.com>
2023-06-24 21:49:36 +0600

    dynamic pagination with seketon ui
    - removed previous design
    - pagination is now dynamic
    - this will be easy for api featch

--- src/routes/teams/teamsLists.svelte
+++ src/routes/teams/teamsLists.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-    import { Table, tableMapperValues } from "@skeletonlabs/skeleton";
-    import type { TableSource } from "@skeletonlabs/skeleton";
+    import { Paginator, Table } from "@skeletonlabs/skeleton";
+    import type { PaginationSettings } from "@skeletonlabs/skeleton";
+
     const teams = [
         {
             name: "Team 1",
@@ -73,75 +74,45 @@
             location: "NYC",
         },
     ];
-    const teams_table: TableSource = {
-        // A list of heading labels.
-        head: ["Name", "Game", "Prize Pool", "Total Game", "Location"],
-        // The data visibly shown in your table body UI.
-        body: tableMapperValues(teams, [
-            "name",
-            "game",
-            "prizePool",
-            "totalGame",
-            "location",
-        ]),
-    };
+    const table_header = [
+        "Name",
+        "Game",
+        "Prize Pool",
+        "Total Game",
+        "Location",
+    ];
+
+    // PaginatorSettings
+    let page = {
+        offset: 0,
+        limit: 5,
+        size: teams.length,
+        amounts: [1, 2, 3, 5, teams.length],
+    } as PaginationSettings;
+    $: sourceBodySliced = teams.slice(
+        page.offset * page.limit,
+        page.offset * page.limit + page.limit
+    );
 </script>
 
 <main class="p-2 mt-3">
-    <Table
-        class="w-full mt-3 table-fixed"
-        source={teams_table}
-        interactive
-        striped
-        bordered
-        hover
-        small
-    />
-    <!-- pagination -->
-    <div class="flex items-center justify-center mt-3">
-        <button
-            class="px-3 py-1 text-gray-100 bg-gray-800 rounded-l hover:bg-gray-700"
-        >
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="inline-block w-5 h-5"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-            >
-                <path
-                    fill-rule="evenodd"
-                    d="M10.707,5.293a1,1,0,0,1,0,1.414L7.414,10l3.293,3.293a1,1,0,1,1-1.414,1.414l-4-4a1,1,0,0,1,0-1.414l4-4a1,1,0,0,1,1.414,0Z"
-                    clip-rule="evenodd"
-                />
-            </svg>
-        </button>
-        <button class="px-3 py-1 text-gray-100 bg-gray-800 hover:bg-gray-700">
-            1
-        </button>
-        <button class="px-3 py-1 text-gray-100 bg-gray-800 hover:bg-gray-700">
-            2
-        </button>
-        <button class="px-3 py-1 text-gray-100 bg-gray-800 hover:bg-gray-700">
-            3
-        </button>
-        <button class="px-3 py-1 text-gray-100 bg-gray-800 hover:bg-gray-700">
-            4
-        </button>
-        <button
-            class="px-3 py-1 text-gray-100 bg-gray-800 rounded-r hover:bg-gray-700"
-        >
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="inline-block w-5 h-5"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-            >
-                <path
-                    fill-rule="evenodd"
-                    d="M9.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 11-1.414-1.414L12.586 10 9.293 6.707a1 1 0 010-1.414z"
-                    clip-rule="evenodd"
-                />
-            </svg>
-        </button>
+    <div class="w-full mt-3">
+        <Table
+            class="w-full mb-5"
+            source={{
+                head: table_header,
+                body: sourceBodySliced.map((team) => [
+                    team.name,
+                    team.game,
+                    team.prizePool,
+                    team.totalGame,
+                    team.location,
+                ]),
+            }}
+            interactive
+            striped
+            hover
+        />
+        <Paginator bind:settings={page} />
     </div>
 </main>
