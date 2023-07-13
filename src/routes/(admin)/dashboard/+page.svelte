f85ce468a6f54fc70ea8f9525b13d4f2d57872f2
Azran Hossain <mr69751@gmail.com>
2023-07-13 19:21:13 +0600

    profile added

--- src/routes/(admin)/dashboard/+page.svelte
+++ src/routes/(admin)/dashboard/+page.svelte
@@ -1,5 +1,56 @@
-<main class="w-3/4 m-auto">
-    <h1 class="text-3xl">Dashboard</h1>
+<main class="w-3/4 p-5 px-32 m-auto rounded-md bg-slate-900">
+    <h1 class="mb-5 text-2xl">Profie</h1>
+    <div class="flex gap-3 pb-5 mb-5 border border-transparent border-b-slate-800">
+        <img
+            src="https://lh3.googleusercontent.com/a/AAcHTtdjOpOKA8dqOAQV8CEfmnw663TkpLXiNc_3DN-xXuYnno8=s96-c"
+            alt="img"
+            class="w-12 rounded-full"
+        />
+        <div>
+            <p class="font-semibold">Azran Hossain</p>
+            <p class="text-sm">azran@gmail.com</p>
+        </div>
+    </div>
+    <div>
+        <main class="flex m-auto">
+            <div>
+                <div class="w-3/4 mr-4">
+                    <label class="block mb-2 text-gray-300" for="name">
+                        Change Name
+                    </label>
+                    <input
+                        class="w-full px-3 py-2 mb-4 leading-tight text-gray-300 border border-gray-700 rounded shadow appearance-none bg-inherit focus:outline-none focus:shadow-outline"
+                        id="name"
+                        type="text"
+                        placeholder="Enter your name"
+                    />
+                </div>
+                <div class="w-3/4">
+                    <label class="block mb-2 text-gray-300" for="password">
+                        Change Password
+                    </label>
+                    <input
+                        class="w-full px-3 py-2 mb-4 leading-tight text-gray-300 border border-gray-700 rounded shadow appearance-none bg-inherit focus:outline-none focus:shadow-outline"
+                        id="password"
+                        type="password"
+                        placeholder="Enter your password"
+                    />
+                    <button
+                        class="px-4 py-2 text-gray-300 bg-gray-600 rounded hover:bg-gray-500 focus:outline-none focus:shadow-outline-blue"
+                        type="button"
+                    >
+                        Save
+                    </button>
+                </div>
+            </div>
+        </main>
+
+        <style>
+            main {
+                color: white;
+            }
+        </style>
+    </div>
 </main>
 
 <style>
